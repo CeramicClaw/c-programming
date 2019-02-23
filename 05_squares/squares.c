cff63839d9e41a932fc85998c6d1c62493d3d184
Coursera Learner <nobody@nowhere.nul>
2019-02-23 13:23:42 +0000

    Finished assignemnt 5

--- 05_squares/squares.c
+++ 05_squares/squares.c
@@ -1,15 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int max(int a, int b) {
+  if (a > b) {
+      return a;
+    }
+  else {
+    return b;
+  }
+}
+
+int between(int a, int b, int c) {
+  // check if a is between b and c
+  if ((a >= b) && (a < c)) {
+      return 1;
+    }
+    else {
+      return 0;
+    }
+}
 
 void squares(int size1, int x_offset, int y_offset, int size2) {
+
   //compute the max of size1 and (x_offset + size2).  Call this w
+  int w = max(size1, (x_offset + size2));
 
   //compute the max of size1 and (y_offset + size2).  Call this h
+  int h = max(size1, (y_offset + size2)); 
 
   //count from 0 to h. Call the number you count with y
+  for (int y = 0; y < h; y++) {
 
     //count from 0 to w. Call the number you count with x
+    for (int x = 0; x < w; x++) {
 
       //check if  EITHER
       //    ((x is between x_offset  and x_offset +size2) AND 
@@ -17,17 +40,26 @@ void squares(int size1, int x_offset, int y_offset, int size2) {
       //  OR
       //    ((y is between y_offset and y_offset + size2) AND
       //     x is equal to either x_offset OR x_offset + size2 -1)
+      if ( ((between(x,x_offset,(x_offset + size2))) && ((y == y_offset) || (y == (y_offset + size2 - 1)))) || ((between(y,y_offset,(y_offset + size2))) && ((x == x_offset) || (x == (x_offset + size2 - 1))))) {     
       // if so, print a *
-
+	printf("*");
+      }
+      else if (((x < size1) && ((y == 0) || (y == (size1 - 1)))) || ((y < size1) && ((x == 0) || x == (size1 - 1)))) {
       //if not,
       // check if EITHER
       //    x is less than size1 AND (y is either 0 or size1-1)
       // OR
       //    y is less than size1 AND (x is either 0 or size1-1)
       //if so, print a #
-
+	printf("#");
+      }
+      else {
+	printf(" ");
+      }
       //else print a space
     //when you finish counting x from 0 to w, 
     //print a newline
-
+    }
+    printf("\n");
+  }
 }
