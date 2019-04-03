2a234c73804aad21216ecaf20fe6bb20e9b2f110
Coursera Learner <nobody@nowhere.nul>
2019-04-03 23:42:47 +0000

    edited test-power.c

--- 22_tests_power/test-power.c
+++ 22_tests_power/test-power.c
@@ -5,7 +5,7 @@ unsigned power(unsigned x, unsigned y);
 
 void run_check(unsigned x, unsigned y, unsigned expected_ans) {
   if (power(x, y) != expected_ans) {
-    printf("Incorrect!\n");
+    printf("Incorrect! %d ^ %d expected %d actual %d\n", x, y, expected_ans, power(x,y));
     exit(EXIT_FAILURE);
   }
 }
@@ -15,5 +15,7 @@ int main (void) {
   run_check(0, 0, 1);
   run_check(-2, 2, 4);
   run_check(-2, 3, -8);
+  unsigned test = power(-2, -1);
+  printf("%d\n",test);
   return EXIT_SUCCESS;
 }
