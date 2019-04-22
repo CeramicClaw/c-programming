afe6c36be6ead65b32208832f1139f4013494ffb
Coursera Learner <nobody@nowhere.nul>
2019-04-22 16:35:17 +0000

    Fixed future.c

--- c4prj2_input/future.c
+++ c4prj2_input/future.c
@@ -5,9 +5,12 @@
 void add_future_card(future_cards_t * fc, size_t index, card_t * ptr) {
   if( (index + 1) > fc->n_decks) { //Incorrect
     fc->decks = realloc(fc->decks, (index + 1) * sizeof(*fc->decks));
-    //(fc->decks[index]).cards = malloc(sizeof(*(fc->decks[index]).cards));
-    //(fc->decks[index]).n_cards = 0;
+    (fc->decks[index]).cards = malloc(sizeof(*(fc->decks[index]).cards));
+    (fc->decks[index]).n_cards = 0;
     fc->n_decks = index + 1;
+  }
+  if (fc->decks[index].n_cards == NULL) {
+      fc->decks[index].n_cards = 0;
   }
   (fc->decks[index]).n_cards++;
   (fc->decks[index]).cards = realloc((fc->decks[index]).cards, (fc->decks[index]).n_cards * sizeof(*(fc->decks[index]).cards));
